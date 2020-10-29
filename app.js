3ba7c7d69e458ef026d7a4475c9292e70a10a153
Andrew Circelli <andrewcircelli@gmail.com>
2020-10-29 09:32:26 -0400

    begin app.js

--- app.js
+++ app.js
@@ -10,12 +10,89 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-const teamMembers = [];
-const arrayId     = [];
+const teamMembers = []
+const arrayId = []
 
+function appMenu() {
+    function  createTeam(){
+        // inquirer to ask which type of employee you want to create and runs the relevant function
+    }
+    function createManager() {
+
+    }
+
+    function createEngineer() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is your engineer's name",
+                validate: answer => {
+                    if(answer !== "") {
+                        return true
+                    }
+
+                    return "Please enter a valid name"
+                }
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "What is your employee's ID?",
+                validate: answer => {
+                    if(answer !== "") {
+                        return true
+                    }
+
+                    return "Please enter a valid ID"
+                }
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is your employee's email?",
+                validate: answer => {
+                    if(answer !== "") {
+                        return true
+                    }
+
+                    return "Please enter a valid email"
+                }
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "What is your employee's Github?",
+                validate: answer => {
+                    if(answer !== "") {
+                        return true
+                    }
+
+                    return "Please enter a valid github"
+                }
+            }
+        ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub)
+            teamMembers.push(engineer)
+            arrayId.push(answers.engineerId)
+            // run a function here that creates the entire "team" prompting you to create another employee
+        })
+    }
+
+    function buildTeam() {
+        // Create the output directory if the output path doesn't exist
+        if (!fs.existsSync(OUTPUT_DIR)) {
+          fs.mkdirSync(OUTPUT_DIR)
+        }
+        fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+      }
+
+      
+}
+
+appMenu()
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
